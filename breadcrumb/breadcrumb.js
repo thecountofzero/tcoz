630a236ce4e34e2d951c8fbd8fcb45de7db2ea81
Mike Malamud <mike.malamud@gmail.com>
2012-07-13 09:35:43 -0400

    Added updateBreadCrumb special event

--- breadcrumb/breadcrumb.js
+++ breadcrumb/breadcrumb.js
@@ -1,11 +1,11 @@
-steal('can/control', 'can/view/ejs', 'can/route', 'can/control/route', function($) {
+steal('can/control', 'can/view/ejs', 'can/route', 'can/control/route', './breadcrumb.css', function($) {
 
     window.TCOZ = window.TCOZ || {};
 
     window.TCOZ.Breadcrumb = can.Control({
         init : function() {
             var self = this;
-            this.setContent(can.route.attr());
+            //this.setContent(can.route.attr());
             //this.element.append('<header class="title breadcrumb"><h5></h5></header>');
             //this.content = this.element.find('h5');
         },
@@ -15,10 +15,14 @@ steal('can/control', 'can/view/ejs', 'can/route', 'can/control/route', function(
             this.element.html(content);
         },
 
-        "{can.route} app": "setContent",
+        //"{can.route} app": "setContent",
 
         setContent: function(data) {
             this._setContent(data.app);
+        },
+
+        " updateBreadcrumb": function(el, ev, data) {
+            this._setContent(data);
         }
     });
 });
